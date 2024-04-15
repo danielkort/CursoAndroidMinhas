e433e7e97a20ab8cc7b79288f66d0349126b08a9
Daniel Kort <daniel1kort@gmail.com>
2024-04-15 13:12:54 -0300

    Uso dos métodos Get e Set.

--- app/src/main/java/devandroid/kort/applistavip/view/MainActivity.java
+++ app/src/main/java/devandroid/kort/applistavip/view/MainActivity.java
@@ -14,6 +14,10 @@ import devandroid.kort.applistavip.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,37 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         pessoa = new Pessoa();
+        // Atribuir conteúdo, dados, valores para o objeto
+        // Conforme o seu modelo, template
+        pessoa.setPrimeiroNome("Daniel");
+        pessoa.setSobreNome("Kort");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("49 991491710");
+
+        outraPessoa.setPrimeiroNome("Kátia");
+        outraPessoa.setSobreNome("Boer");
+        outraPessoa.setCursoDesejado("Ingles");
+        outraPessoa.setTelefoneContato("49 991410891");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
